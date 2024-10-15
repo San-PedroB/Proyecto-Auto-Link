b53f817ea5cb46d1e6b6186a8150004bd0f8cd54
sanPedro <2pedro.beltran@gmail.com>
2024-10-14 21:20:41 -0300

    firestore implementado

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
-import { ToastController, ModalController, NavController } from '@ionic/angular';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import {
+  ToastController,
+  ModalController,
+  NavController,
+} from '@ionic/angular';
 import { FormularioService } from '../../services/formulario.service'; // Importar el servicio
 
+import { FirestoreService } from '../../services/firestore/firestore.service';
 
 @Component({
   selector: 'app-registro',
@@ -10,48 +20,54 @@ import { FormularioService } from '../../services/formulario.service'; // Import
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-
   formularioRegistro: FormGroup;
 
-  constructor(public modalController: ModalController, public fb: FormBuilder, private toastController:ToastController,
-     private formularioService: FormularioService, private navController: NavController) {
+  constructor(
+    public modalController: ModalController,
+    public fb: FormBuilder,
+    private toastController: ToastController,
+    private formularioService: FormularioService,
+    private navController: NavController,
+    private firestoreService: FirestoreService
+  ) {
     this.formularioRegistro = this.fb.group({
-      "nombre": new FormControl("", Validators.required),
-      "apellido": new FormControl("", Validators.required,),
-      "email": new FormControl("", [Validators.required, Validators.email]),
-      "password": new FormControl("", Validators.required),
-      "confirmarPassword": new FormControl("", Validators.required)
-    })
-   }
+      nombre: new FormControl('', Validators.required),
+      apellido: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      confirmarPassword: new FormControl('', Validators.required),
+    });
+  }
 
-  async registrarse(){
-    if(this.formularioRegistro.invalid){
+  async registrarse() {
+    if (this.formularioRegistro.invalid) {
       const toastError = await this.toastController.create({
-        message: "Porfavor completa correctamente todos los campos", 
+        message: 'Porfavor completa correctamente todos los campos',
         duration: 3000,
-        position: 'bottom'
+        position: 'bottom',
       });
       await toastError.present();
       return;
     }
     this.enviarFormulario();
-   }
+  }
 
   async enviarFormulario() {
     if (this.formularioRegistro.valid) {
-      this.formularioService.setDatos(this.formularioRegistro.value);
+      await this.firestoreService.createDocument(
+        'users', //nombre de la coleccion
+        this.formularioRegistro.value
+      );
       console.log('Datos guardados:', this.formularioRegistro.value);
       const toastRegistro = await this.toastController.create({
-        message: "¡Usuario creado exitosamente!", 
+        message: '¡Usuario creado exitosamente!',
         duration: 3000,
-        position: 'bottom'
-      })
+        position: 'bottom',
+      });
       await toastRegistro.present();
       this.navController.navigateRoot('/login');
-    } 
+    }
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
